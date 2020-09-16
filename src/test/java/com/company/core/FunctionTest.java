091b2d7fc78451e924b407a9433b4582eccea53e
Saeed Ashraghi <saeed.ashraghi@queo.swiss>
2020-09-16 09:39:38 +0200

    assertj added

--- src/test/java/com/company/core/FunctionTest.java
+++ src/test/java/com/company/core/FunctionTest.java
@@ -1,21 +1,24 @@
 package com.company.core;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class FunctionTest {
 
     @DisplayName("Test MessageService.get()")
     @Test
     void testGet() {
-        assertEquals("Hello JUnit 5", MessageService.get());
+
+        assertThat(MessageService.get()).isEqualTo("Hello JUnit 5");
     }
 
     @Test
@@ -25,6 +28,9 @@ public class FunctionTest {
         Function<String, Integer> nameMappingFunction = String::length;
 
         final List<Integer> integers = names.stream().map(nameMappingFunction).collect(Collectors.toList());
+
+        assertThat(integers.size() == 5).isTrue();
+        assertThat(integers.get(4)).isEqualTo(7);
 
         System.out.println(integers);
     }
