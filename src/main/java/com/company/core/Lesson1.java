99621a3a652b294a799644d461e6703841addf12
saeedashraghi <lesamuraii@yahoo.de>
2020-09-23 15:37:40 +0200

    new Stream tests

---
+++ src/main/java/com/company/core/Lesson1.java
@@ -0,0 +1,120 @@
+package com.company.core;
+
+import java.util.*;
+import java.util.function.BiFunction;
+
+public class Lesson1 {
+
+    public void runExercises() {
+        System.out.println("JDK 8 Lambdas and Streams MOOC Lesson 1");
+        System.out.println("Running exercise 1 solution...");
+        exercise1();
+        System.out.println("Running exercise 2 solution...");
+        exercise2();
+        System.out.println("Running exercise 3 solution...");
+        exercise3();
+        System.out.println("Running exercise 4 solution...");
+        exercise4();
+        System.out.println("Running exercise 5 solution...");
+        exercise5();
+    }
+
+    /**
+     * All exercises should be completed using Lambda expressions and the new
+     * methods added to JDK 8 where appropriate. There is no need to use an
+     * explicit loop in any of the code. Use method references rather than full
+     * lambda expressions wherever possible.
+     */
+    /**
+     * Exercise 1
+     * <p>
+     * Create a string that consists of the first letter of each word in the list
+     * of Strings provided.
+     */
+    private void exercise1() {
+        List<String> list = Arrays.asList(
+                "alpha", "bravo", "charlie", "delta", "echo", "foxtrot");
+
+        /* YOUR CODE HERE */
+        list.forEach(s -> System.out.println(s.charAt(0)));
+    }
+
+    /**
+     * Exercise 2
+     * <p>
+     * Remove the words that have odd lengths from the list.
+     */
+    private void exercise2() {
+        List<String> list = new ArrayList<>(Arrays.asList(
+                "alpha", "bravo", "charlie", "delta", "echo", "foxtrot"));
+
+        /* YOUR CODE HERE */
+        //list.forEach(s -> {if(s.length() % 2== 1) System.out.println(s);});
+        list.removeIf(s -> s.length() % 2 == 0);
+        list.forEach(System.out::println);
+
+
+    }
+
+    /**
+     * Exercise 3
+     * <p>
+     * Replace every word in the list with its upper case equivalent.
+     */
+    private void exercise3() {
+        List<String> list = new ArrayList<>(Arrays.asList(
+                "alpha", "bravo", "charlie", "delta", "echo", "foxtrot"));
+
+        /* YOUR CODE HERE */
+        list.forEach(s -> System.out.println(s.toUpperCase()));
+    }
+
+    /**
+     * Exercise 4
+     * <p>
+     * Convert every key-value pair of the map into a string and append them all
+     * into a single string, in iteration order.
+     */
+    private void exercise4() {
+        Map<String, Integer> map = new TreeMap<>();
+        map.put("c", 3);
+        map.put("b", 2);
+        map.put("a", 1);
+
+        /* YOUR CODE HERE */
+        StringBuilder sb = new StringBuilder();
+        //map.forEach((String key, Integer value) -> sb.append(key).append(" - ").append(value).append("; "));
+        //System.out.println(sb.toString());
+
+        BiFunction<String, Integer, String> biFunction = (String key, Integer value) -> {
+            return sb.append(key).append(" - ").append(value).append("; ").toString();
+        };
+        map.forEach((String key, Integer value) -> biFunction.apply(key, value));
+        System.out.println(sb.toString());
+    }
+
+    /**
+     * Exercise 5
+     * <p>
+     * Create a new thread that prints the numbers from the list.
+     */
+    private void exercise5() {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        /* YOUR CODE HERE */
+        new Thread(() -> {
+            list.forEach(System.out::println);
+        }).start();
+
+    }
+
+    /**
+     * Main entry point for application
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Lesson1 lesson = new Lesson1();
+        lesson.runExercises();
+    }
+}
